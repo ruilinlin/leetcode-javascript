a36487e0953b5b2f0314318e583b2754f6151f75
孙瑞琳 <sunruilin@rins-MacBook-Air.local>
2024-11-20 17:26:11 -0800

    2461. Maximum Sum of Distinct Subarrays With Length K

--- 2461. Maximum Sum of Distinct Subarrays With Length K.js
+++ 2461. Maximum Sum of Distinct Subarrays With Length K.js
@@ -1,34 +1,35 @@
 /**
  * single pointer + outer storage
- * time complex: O(0) space complex: O(0)
+ * time complex: O(n) space complex: O(k)
  * @param {number[]} nums
  * @param {number} k
  * @returns {number}
  */
 
-var maximumSubarraySum = function(num, k){
+var maximumSubarraySum = function(nums, k){
 
   let maxSum = 0;
   let currentSum = 0;
-
-  // notice the difference between set and array(https://dev.to/0shuvo0/array-vs-set-in-javascript-4h50)
- //use set。length as a certification of the maxmum summary
-  let set = new Set();
   let leftIndex = 0;
 
-  for(let i = 0; i < num.length; i++){
-    currentSum += num[i];
-    set.add(num[i]);
 
-    if (i-leftIndex + 1 == k){
-      // remove the left element from the current sum
-      currentSum -= num[leftIndex];
-      set.delete(num[leftIndex]);
-      leftIndex++;
+// notice the difference between set and array(https://dev.to/0shuvo0/array-vs-set-in-javascript-4h50)
+//use set。length as a certification of the maxmum summary
+ let set = new Set();
+
+  for (let i = 0; i < nums.length; i++){
+    currentSum += nums[i];
+    set.add(nums[i]);
+
+    if ( i - leftIndex + 1 > k){
+        // remove the leftest num in nums
+        set.delete(nums[leftIndex]);
+        currentSum -= nums[leftIndex];
+        leftIndex ++;
     }
 
-    if(i-leftIndex + 1 == k && set.length == k){
-      maxSum = Math.max(maxSum, currentSum);
+    if (i - leftIndex + 1 == k && set.size == k){
+        maxSum = Math.max(maxSum, currentSum);
     }
 
     return maxSum;
