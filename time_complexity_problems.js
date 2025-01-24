bcb977088a4ffbbb1d8633f1952e3736d195014b
孙瑞琳 <sunruilin@rins-MacBook-Air.local>
2025-01-24 12:19:58 -0800

    tape equalibrium problem

--- time_complexity_problems.js
+++ time_complexity_problems.js
@@ -1,4 +1,5 @@
 //FROGJMP
+// time complexity O(1)
 function solution(A,B,K){
  const distance = B-A; 
  const jumps = Math.ceil(distance/K);
@@ -6,9 +7,24 @@ function solution(A,B,K){
 }
 
 //PermMissingElem
+// time complexity O(N) or O(N*logN)
 function solution(A){
   const actualSum = A.reduce((sum, num) => sum + num, 0);
   const expectedSum = (A.length+1)*(A.length+2)/2
   const missNum = expectedSum - actualSum;
   return missNum;
+}
+
+//tape equalibrium
+function solution(A){
+   let leftSum = 0;
+   const totalSum = A.reduce((sum, num) => sum + num, 0);
+   let minDiff = Infinity;
+   for ( i = 0; i < A.length -1; i ++){
+    leftSum += A[i];
+    rightSum =  totalSum - leftSum;
+    const diff = Math.abs(leftSum - rightSum);
+    minDiff = Math.min(minDiff, diff);
+   }
+   return minDiff;
 }
