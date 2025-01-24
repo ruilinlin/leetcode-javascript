030ac41106b1217ee89ae2dc34eb78acb56bb04c
孙瑞琳 <sunruilin@rins-MacBook-Air.local>
2025-01-24 12:02:53 -0800

    permminssingnum problem

--- time_complexity_problems.js
+++ time_complexity_problems.js
@@ -1,10 +1,14 @@
 //FROGJMP
 function solution(A,B,K){
- let count = 1;
- for (let i = A; i <= B ; i++){
-  if(i+count*K <= B){
-    count++;
-  }
- }
- return count;
+ const distance = B-A; 
+ const jumps = Math.ceil(distance/K);
+ return jumps;
+}
+
+//PermMissingElem
+function solution(A){
+  const actualSum = A.reduce((sum, num) => sum + num, 0);
+  const expectedSum = (A.length+1)*(A.length+2)/2
+  const missNum = expectedSum - actualSum;
+  return missNum;
 }
