6b5f05f4eade174f399fe5cffd5d66d7fd208ab8
孙瑞琳 <sunruilin@rins-MacBook-Air.local>
2025-01-24 15:24:35 -0800

    add percheck problem

--- count_problems.js
+++ count_problems.js
@@ -10,3 +10,21 @@ function solution(X,A){
   }
   return -1;
 }
+
+//permCheck
+function solution(A){
+  const setCheck = new Set(A);
+  // const expectedSum = (A.length + 1) *(A.length + 2) / 2;
+  // const actualSum = A.reduce((sum,num) =>{sum + num}, 0);
+  // if ( setCheck.size !== A.length || expectedSum !== actualSum){
+  //   return 0;
+  // }
+  const n = A.length;
+
+  for(let i =0; i< n; i++){
+    if(!setCheck.has(i)){
+      return 0;
+    }
+  }
+  return 1;
+}
