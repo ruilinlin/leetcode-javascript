a0f4a300004dc294ea4b8a8ba7c137840806ddf2
孙瑞琳 <sunruilin@rins-MacBook-Air.local>
2025-01-25 15:49:34 -0800

    add the maxCounter problem

--- count_problems.js
+++ count_problems.js
@@ -11,20 +11,40 @@ function solution(X,A){
   return -1;
 }
 
+
 //permCheck
 function solution(A){
   const setCheck = new Set(A);
-  // const expectedSum = (A.length + 1) *(A.length + 2) / 2;
-  // const actualSum = A.reduce((sum,num) =>{sum + num}, 0);
-  // if ( setCheck.size !== A.length || expectedSum !== actualSum){
-  //   return 0;
-  // }
+//   const n = A.length;
+
+//   // 计算预期的等差数列和（从 1 到 n）
+//   const expectedSum = (n * (Math.min(...A) + Math.max(...A))) / 2;
+//   const actualSum = A.reduce((sum,num) =>{sum + num}, 0);
+//   if ( setCheck.size === A.length && expectedSum === actualSum &&Math.max(...A) === n && Math.min(...A) === 1){
+//     return 1;
+//   }
   const n = A.length;
 
-  for(let i =0; i< n; i++){
-    if(!setCheck.has(i)){
-      return 0;
-    }
+  if (setCheck.size === n && Math.max(...A) === n && Math.min(...A) === 1) {
+    return 1;
+  } else {
+    return 0;
   }
-  return 1;
+return 0;
 }
+
+//maxCounters
+function solution(N,A){
+  let result = new Array(N).fill(0);
+  let maxCounter =0;
+  for(let i =0; i < A.length; i++){
+    if(A[i] === N +1){
+      // result = result.map(() => maxCounter);
+      result = result.fill(maxCounter);
+    } else{
+      result[A[i]] +=1;
+      maxCounter ++;
+      }
+    }
+    return result;
+  }
