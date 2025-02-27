e0da8721b5098b55513eed71761d2b288f74a919
孙瑞琳 <sunruilin@rins-MacBook-Air.local>
2025-02-27 15:21:38 -0800

    adding the minperimeterrectangle problem

--- primandcomposite_numbers_problems.js
+++ primandcomposite_numbers_problems.js
@@ -11,3 +11,18 @@ function solution(N){
   }
   return count;
 }
+
+// minperimeterrectangle
+function solution(N){
+  // find the factors of N
+  let factors = [];
+  for(let i = 1; i * i <= N; i++){
+    if (N%i === 0){
+      factors.push(i);
+    }
+  }
+  // find the largest factor
+  let largestFactor = factors[factors.length -1];
+  let anotherFactor = N/largestFactor;
+  return 2*(largestFactor + anotherFactor);
+}
